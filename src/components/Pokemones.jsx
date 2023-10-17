33fb6cd5fff85403d08dcde6b859676963ad11e1
Jlandav <98247950+JLanda003@users.noreply.github.com>
2023-10-16 23:09:02 -0500

    //Consumir Api, botonera
    Consumir una Api, y cambiar el estatus con Botones

--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -3,24 +3,38 @@ import React, { useEffect, useState } from 'react'
 const Pokemones = () => {
 
   const [pokemones, setPokemones] = useState([]);
+  const [anterior, setAnterior] = useState(null);
+  const [siguiente, setSiguiente] = useState(null);
+  const [actual, setActual] = useState('https://pokeapi.co/api/v2/ability/?limit=10&offset=0');
 
   useEffect(() => {
     const obtenerPokemones = async () =>{
-      const response = await fetch('https://pokeapi.co/api/v2/ability/?limit=10&offset=10');
+      const response = await fetch(actual);
       const data = await response.json();
 
-      setPokemones(data.results);
+      console.log(data);
 
-      console.log(pokemones);
+      setPokemones(data.results);
+      setAnterior(data.previous);
+      setSiguiente(data.next);
+
     }
     
     obtenerPokemones();
-  }, []);
+  }, [actual]);
 
   return (
-    pokemones.map((pokemones, index) => {
-      return <p>{pokemones.name}</p>
-    })
+    <div>
+      <ul>
+      {
+        pokemones.map((pokemones, index) => {
+          return <li key={index}>{ pokemones['name'] }</li>
+        })
+      }
+      </ul>
+      <button onClick={() => anterior !== null && setActual(anterior)}>Anterior</button>
+      <button onClick={() => siguiente !== null && setActual(siguiente)}>Siguiente</button>
+    </div>
   )
 }
 
