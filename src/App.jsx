47e112ff5d22930b44cec6e3cf9447b6d7a14303
Jlandav <98247950+JLanda003@users.noreply.github.com>
2023-10-21 15:31:42 -0500

    //Ruteo
    Implementacion de uso de rutas, para paginado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './styles/App.css'
-import 'bootstrap/dist/css/bootstrap.min.css' // Se debe de importar los css de bootstrap
-import CrudApi from './components/crud/CrudApi'
-// import CrudApp from './components/crud/CrudApp'
-// import Text from './components/Texto';
-// import Contador from './components/Contador';
-// import Bootstrap from './components/Bootstrap';
-// import NavScrollExample from './components/Navbar'
-// import Pokemones from './components/Pokemones';
-// import Estilos from './components/Estilos';
-// import ListaNombre from './components/ListaNombre'
-// import Referencia from './components/Referencia';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Se debe de importar los css de bootstrap
+import CrudApi from './components/crud/CrudApi';
+import CrudApp from './components/crud/CrudApp';
+import Text from './components/Texto';
+import Contador from './components/Contador';
+import Bootstrap from './components/Bootstrap';
+import NavScrollExample from './components/Navbar';
+import Pokemones from './components/Pokemones';
+import Estilos from './components/Estilos';
+import ListaNombre from './components/ListaNombre';
+import Referencia from './components/Referencia';
+import { Container } from 'react-bootstrap';
 
 // class App extends React.Component{
 //   render(){
@@ -21,24 +23,33 @@ import CrudApi from './components/crud/CrudApi'
 const App = () => {
   return(
       <div className='container'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/condator' element={ <Contador /> } />
+            <Route path='/listanombre' element={ <ListaNombre /> } />
+            <Route path='/pokemones' element={ <Pokemones /> } />
+            <Route path='/referencias' element={ <Referencia /> } />
+            <Route path='/estilos' element={ <Estilos /> } />
+            <Route path='/boostrap' element={ <Bootstrap /> } />
+            <Route path='/navscroll' element={ <NavScrollExample /> } />
+            <Route path='/crudapp' element={ <CrudApp /> } />
+            <Route path='/crudapi' element={ <CrudApi /> } />
+            <Route path='*' element={<h2>Curso React</h2>} />
+          </Routes>
+        </BrowserRouter>
         {/* <Text
         name="Joshua"
         apellido="Landa"
         edad="23" />
-        <hr />
         <ListaNombre />
-        <hr />
-        <Contador />
-        <hr /> 
+        <Contador /> 
         <Pokemones />
-        <hr />
         <Referencia />
         <Estilos />
-        // <Bootstrap /> Antigua forma
+        <Bootstrap /> {//Antigua forma}
         <NavScrollExample />
         <CrudApp />
-        */}
-        <CrudApi />
+        <CrudApi /> */}
       </div>
     )
 }
